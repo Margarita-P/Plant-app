2714e05c2a9d0b4526a01a14401e14b92b360006
Margarita-P <79039786+Margarita-P@users.noreply.github.com>
2023-05-18 17:29:21 +0300

    Add files via upload

--- FirstPage.js
+++ FirstPage.js
@@ -1,55 +1,99 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Modal, Image } from 'react-native';
+import {ScrollView, View, Text, TouchableOpacity, Modal, Image, Dimensions } from 'react-native';
+import plantData from './plantsData/';
 
 const FirstPage = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
+  const [selectedPlant, setSelectedPlant] = React.useState(null);
 
-  const handleImagePress = () => {
+  const handleImagePress = (plant) => {
     setModalVisible(!modalVisible);
+    setSelectedPlant(plant);
   };
+
+  const screenWidth = Dimensions.get('window').width;
 
   const styles = {
     container: {
-      flex: 1,
+      width: screenWidth / 2,
       alignItems: 'center',
       justifyContent: 'center',
+      paddingVertical: 10,
+    },
+
+    row: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+    },
+
+    lipstickPlantContainer: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      //paddingVertical: 10,
+    },
+
+    lipstickPlant: {
+      width: 200,
+      height: 200,
     },
     modalContainer: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
     },
-    lipstickPlantContainer: {
-      position: 'absolute',
-      top: 20,
-      left: 20,
-      zIndex: 1,
+
+    modalContent: {
+      backgroundColor: "#DFB79E",      
+      padding: 20,
+      borderRadius: 10,
+      maxWidth: 300,
     },
-    lipstickPlant: {
-      width: 150,
-      height: 150,
+
+    titleText:{
+      fontSize: 20,
+      color: '#F5F5F5',
+    },
+    
+    dataText: {
+      fontSize: 15,
+      color: '#F5F5F5',
     },
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleImagePress} style={styles.lipstickPlantContainer}>
-        <Image  source={require('./assets/lipstickplant.jpg')} style={styles.lipstickPlant} />
-      </TouchableOpacity>
+    <ScrollView>
+      <View style={styles.row}>
+        {plantData.map((plant) => (
+          <View style={styles.container} key={plant.id}>
+            <TouchableOpacity onPress={() => handleImagePress(plant)} style={styles.lipstickPlantContainer}>
+              <Image source={plant.image} style={styles.lipstickPlant} />
+              <Text>{plant.name}</Text>
+            </TouchableOpacity>
 
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <Text>This is the pop-up page!</Text>
-          {/* Add more content here */}
-        </View>
-      </Modal>
-    </View>
+            <Modal
+              visible={modalVisible}
+              animationType="slide"
+              transparent={true}
+              onRequestClose={() => setModalVisible(false)}
+            >
+              <View style={styles.modalContainer}>
+                {selectedPlant && (
+                  <View style={styles.modalContent}>
+                    <Text style={styles.dataText}>{selectedPlant.name}</Text>
+                    <Text style={styles.dataText}>{selectedPlant.description}</Text>
+                    <Text style={styles.dataText}>{selectedPlant.wateringInstructions}</Text>
+                    <Text style={styles.dataText}>{selectedPlant.lightRequirements}</Text>
+                    {/* Add more content here */}
+                  </View>
+                )}
+              </View>
+            </Modal>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ plantsData.js
@@ -0,0 +1,69 @@
+const plantData = [
+  {
+    id: 1,
+    name: 'Orchidėja',
+    image: require('./assets/plants/orchid2.jpg'),
+    description: 'Egzotiška kambarinė gėlė.',
+    wateringInstructions: 'Laistykite augalą kas 1 ar 2 savaites, tačiau svarbu stebėti šaknis – kai jos pilkšvos, gėlė yra ištroškusi',
+    lightRequirements: 'Mėgsta daug netiesioginės saulės šviesos',
+  },
+  {
+    id: 3,
+    name: 'Aglaonema',
+    image: require('./assets/plants/aglaonema.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 4,
+    name: 'Filodendras',
+    image: require('./assets/plants/filodendras.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 5,
+    name: 'Kalateja',
+    image: require('./assets/plants/Kalateja.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 6,
+    name: 'Monstera',
+    image: require('./assets/plants/Monstera.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 7,
+    name: 'Sansevjera',
+    image: require('./assets/plants/Sansevjera.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 8,
+    name: 'Skindapas',
+    image: require('./assets/plants/Skindapas.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  {
+    id: 9,
+    name: 'Aukštoji aspidistra',
+    image: require('./assets/plants/Aukstojiaspidistra2.jpg'),
+    description: 'Irasyti',
+    wateringInstructions: 'Irasyti',
+    lightRequirements: 'Irasyti',
+  },
+  // Add more plant objects as needed
+];
+
+export default plantData;
